7bb9e78d476bce571d931910cc4dc3846509bf63
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-16 15:51:13 -0600

    feat(admin): cambiar componente de submit button

--- src/shared/components/submit-button.tsx
+++ src/shared/components/submit-button.tsx
@@ -1,21 +1,23 @@
 'use client'
 
-import { Button } from "@tremor/react";
+import { Button, ButtonProps } from "@tremor/react";
 import { useFormStatus } from "react-dom";
 
 export const SubmitButton = ({
   textStatic,
   textPending,
   className,
+  color,
 }: {
   textStatic: string;
   textPending: string;
   className?: string;
+  color?: ButtonProps["color"];
 }) => {
   const { pending } = useFormStatus();
 
   return (
-    <Button type="submit" className={className} disabled={pending} color="sky">
+    <Button type="submit" className={className} disabled={pending} color={color ?? "sky"}>
       {pending ? textPending : textStatic}
     </Button>
   );
