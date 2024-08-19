befe767198d31094b513b5eb52efd453462724c1
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-19 10:48:59 -0600

    feat(admin): agregar asesores en nav link

--- src/features/layout/data/nav-links.ts
+++ src/features/layout/data/nav-links.ts
@@ -1,6 +1,7 @@
-import { HomeIcon, UsersIcon } from "lucide-react";
+import { ContactIcon, HomeIcon, UsersIcon } from "lucide-react";
 
 export const navLinks = [
   { name: 'Inicio', href: '/dashboard', icon: HomeIcon },
   { name: 'Cuentas', href: '/dashboard/accounts', icon: UsersIcon },
+  { name: 'Asesores', href: '/asesores', icon: ContactIcon },
 ];
