c791b8938399a79eec7a55ee994444a4f7c6b81b
Ulises Vargas <uli.vargas02@gmail.com>
2024-08-21 05:02:30 -0600

    styles: agrego espacio al final del contenido principal

--- src/features/layout/layouts/mainlayout.tsx
+++ src/features/layout/layouts/mainlayout.tsx
@@ -12,8 +12,8 @@ export const MainLayout = ({ children }: { children: React.ReactNode }) => {
           <Header />
         </div>
         <main className="flex-grow overflow-y-auto py-1 md:py-3 px-4 text-tremor-content dark:text-tremor-content-subtle">
-          <div className="pb-10 flex h-full flex-col gap-6">
-          {children}
+          <div className="pb-10">
+            <div className="flex h-full flex-col gap-6">{children}</div>
           </div>
         </main>
       </div>
