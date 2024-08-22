e67cf92f69265063ad006939ce041085cbd29d68
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-22 17:21:31 -0600

    refactor: separar codigo, agregar zustand y crear hook de delete advisor

--- src/features/advisors/components/list-buttons-advisors.tsx
+++ src/features/advisors/components/list-buttons-advisors.tsx
@@ -4,7 +4,6 @@ import { SubmitButton } from "@/shared/components/submit-button";
 import {
   Badge,
   Button,
-  Callout,
   Dialog,
   DialogPanel,
   Select,
@@ -15,18 +14,19 @@ import { Pencil, Trash2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Advisor } from "../types/advisor";
 import { UserStatus } from "@prisma/client";
-import { deleteAdvisor } from "../actions/delete-advisor";
 import { useFormState } from "react-dom";
 import { editAdvisor } from "../actions/edit-advisor";
 import { useNotificationStore } from "@/features/notification/store/notification-store";
 import { Modal } from "@/shared/components/modal";
+import { useDeleteAdvisor } from "../hooks/use-delete-advisor";
+import { DeleteAdvisorForm } from "./delete-advisor-form";
 
 export const ListButtonsAdvisors = ({
   advisor,
 }: {
   advisor: Advisor;
 }): JSX.Element => {
-  const [isOpenModalDelete, setIsOpenModalDelete] = useState<boolean>(false);
+  const { advisorIdToDelete, openDeleteModalAdvisor, closeDeleteModalAdvisor } = useDeleteAdvisor();
   const [isOpenModalEdit, setIsOpenModalEdit] = useState<boolean>(false);
   const { showNotification } = useNotificationStore((state) => state);
   const [state, formAction] = useFormState(editAdvisor, {
@@ -38,15 +38,6 @@ export const ListButtonsAdvisors = ({
       general: "",
     },
   });
-
-  const handleDeleteAdvisor = async (): Promise<void> => {
-    const isOperationConfirmed: boolean = await deleteAdvisor(advisor.id);
-    setIsOpenModalDelete(false);
-
-    if (isOperationConfirmed)
-      return showNotification("¡Asesor creado exitosamente!", "success");
-    showNotification("Error desconocido", "error");
-  };
 
   useEffect(() => {
     if (state.errors === null) {
@@ -66,7 +57,7 @@ export const ListButtonsAdvisors = ({
 
   return (
     <>
-      <Button color="red" onClick={() => setIsOpenModalDelete(true)}>
+      <Button color="red" onClick={() => openDeleteModalAdvisor(advisor.id)}>
         <Trash2 size={20} />
       </Button>
       <Button color="blue" onClick={() => setIsOpenModalEdit(true)}>
@@ -74,64 +65,13 @@ export const ListButtonsAdvisors = ({
       </Button>
       {/* ! Delete modal */}
       <Modal
-        isOpen={isOpenModalDelete}
-        setIsOpen={setIsOpenModalDelete}
-        title="Eliminar asesor"
+        isOpen={advisor.id === advisorIdToDelete}
+        setIsOpen={closeDeleteModalAdvisor}
+        title="¿Estás seguro de que deseas eliminar este registro?"
+        description="Esta acción no se puede deshacer. Una vez eliminado, el registro será eliminado permanentemente de la base de datos. Por favor, asegúrate de que realmente deseas proceder con esta acción."
+        size="md"
       >
-        <h3 className="text-lg font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-          ¿Estás seguro de que deseas eliminar este registro?
-        </h3>
-        <p className="mt-2 leading-6 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-          Esta acción no se puede deshacer. Una vez eliminado, el registro será
-          eliminado permanentemente de la base de datos. Por favor, asegúrate de
-          que realmente deseas proceder con esta acción.
-        </p>
-
-        <Callout title="Asesor a eliminar" className="mt-4" color="red">
-          <div className="flex flex-row gap-2 mb-1">
-            <p>Nombre:</p>
-            <p className="font-semibold">{advisor?.name}</p>
-          </div>
-          <div className="flex flex-row gap-2 mb-1">
-            <p>Email:</p>
-            <p className="font-semibold">{advisor?.email}</p>
-          </div>
-          <div className="flex flex-row gap-2 mb-1">
-            <p>Status:</p>
-            <p>
-              {advisor.status === UserStatus.ACTIVE ? (
-                <Badge color="green">Activo</Badge>
-              ) : (
-                <Badge color="red">Inactivo</Badge>
-              )}
-            </p>
-          </div>
-          <div className="flex flex-row gap-2">
-            <p>Fecha de creación:</p>
-            <p className="font-semibold">
-              {advisor?.createdAt.toLocaleString()}
-            </p>
-          </div>
-        </Callout>
-
-        <div className="mt-8 w-full flex flex-row gap-2">
-          <SubmitButton
-            textStatic="Eliminar"
-            color="red"
-            className="flex-1"
-            textPending="Eliminando..."
-            onClick={handleDeleteAdvisor}
-          />
-          <Button
-            variant="secondary"
-            color="red"
-            type="button"
-            className="flex-1"
-            onClick={() => setIsOpenModalDelete(false)}
-          >
-            Cancelar
-          </Button>
-        </div>
+        <DeleteAdvisorForm advisor={advisor} />
       </Modal>
 
       {/* ! Edit modal */}
