c60a2290f4ef5ff332ea434a7df786fd3feca1be
Ulises Vargas <uli.vargas02@gmail.com>
2024-09-13 17:52:27 -0600

    styles: se agranda tamaño de iconos de la quote card

--- src/features/home/components/quote-card.tsx
+++ src/features/home/components/quote-card.tsx
@@ -12,17 +12,17 @@ export const QuoteCard = ({
   description: string;
 }) => {
   return (
-    <div className="w-full border max-w-xs sm:w-72 h-auto sm:h-80 bg-white rounded-lg shadow-md overflow-hidden transition-all duration-300 sm:hover:shadow-xl relative group">
+    <button onClick={() => console.log("Ver más")} className="w-full border max-w-xs sm:w-72 h-auto sm:h-80 bg-white rounded-lg shadow-md overflow-hidden transition-all duration-300 sm:hover:shadow-xl relative group">
       <div className="absolute inset-0 bg-gradient-to-b from-[#008AED] to-[#004E87] opacity-0 sm:group-hover:opacity-70 transition-opacity duration-300 z-10"></div>
 
       <div className="flex flex-col items-center sm:justify-center h-full p-6 relative z-20">
         <div className="transition-all duration-300 transform sm:group-hover:-translate-y-[60%] flex flex-col items-center">
-          <div className="w-24 h-24 bg-blue-100 rounded-full flex items-center justify-center mb-1 sm:group-hover:scale-90 transition-transform duration-300">
+          <div className="w-36 h-36 group-hover:w-24 group-hover:h-24 bg-blue-100 rounded-full flex items-center justify-center mb-1 transition-transform duration-300">
             <Image
               src={image}
-              width={80}
-              height={80}
-              className="object-cover"
+              width={110}
+              height={110}
+              className="object-cover group-hover:w-[80px]"
               alt={`Imagen de ${title}`}
             />
           </div>
@@ -35,23 +35,22 @@ export const QuoteCard = ({
           <p className="text-gray-800 sm:text-white mb-3 text-sm">
             {description}
           </p>
-          <button
-            onClick={() => console.log("Ver más")}
+          <span
             className="mt-2 inline-flex items-center justify-center w-full sm:w-auto text-blue-500 sm:text-white transition-colors duration-300 sm:bg-white sm:bg-opacity-20 hover:bg-opacity-10 px-4 py-2 rounded text-"
           >
             Ver más
             <ArrowRight className="ml-2 w-4 h-4" />
-          </button>
+          </span>
         </div>
       </div>
 
-      <div className="absolute top-0 -right-1 w-20 h-20 overflow-hidden transition-opacity duration-300 sm:group-hover:opacity-0">
-        <div className="absolute top-0 right-0 w-6 h-6 bg-blue-500 rotate-45 transform origin-bottom-left"></div>
+      <div className="absolute top-0 -right-2.5 w-20 h-20 overflow-hidden transition-opacity duration-300 sm:group-hover:opacity-0">
+        <div className="absolute top-0 right-0 w-8 h-8 bg-blue-500 rotate-45 transform origin-bottom-left"></div>
         <div className="absolute top-0 right-0 w-20 h-20">
-          <div className="absolute top-[2px] right-[1px] w-4 h-4 bg-white rounded-full animate-ping"></div>
-          <div className="absolute top-[2px] right-[1px] w-4 h-4 bg-white rounded-full"></div>
+          <div className="absolute top-[2px] -right-[2px] w-6 h-6 bg-white rounded-full animate-ping"></div>
+          <div className="absolute top-[2px] -right-[2px] w-6 h-6 bg-white rounded-full"></div>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
